e05d1f1b9570d1a3ba5a80765c70c4a2c3f0d1c2
seongjunme <72444675+seongjunme@users.noreply.github.com>
2021-12-14 19:02:42 +0900

    fix: section1/7

--- section1/7.js
+++ section1/7.js
@@ -1,14 +1,4 @@
-const solution = (day, cars) => {
-  let cnt = 0;
-
-  for (const car of cars) {
-    if (car % 10 === day) {
-      cnt += 1;
-    }
-  }
-
-  return cnt;
-};
+const solution = (day, cars) => cars.filter((car) => car % 10 === day).length;
 
 console.log(solution(3, [25, 23, 11, 47, 53, 17, 33]));
 console.log(solution(0, [12, 20, 54, 30, 87, 91, 30]));
